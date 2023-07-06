4deb71f5f5910a36fc6fd297b213dc60e1aca44a
jbfiedle <38211953+jbfiedle@users.noreply.github.com>
2023-07-06 05:48:12 -0400

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -6,10 +6,10 @@ and SQL database
 """
 import sys
 from flask import Flask
-from service import config
-from service.common import log_handlers
 from flask_talisman import Talisman
 from flask_cors import CORS
+from service import config
+from service.common import log_handlers
 
 # Create Flask application
 app = Flask(__name__)
